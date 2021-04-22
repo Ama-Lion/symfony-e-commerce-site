f39c2e7182030d4c14a514c9134a24d29fce5ccd
‘Ama_lion’ <bahamadu2019@gmail.com>
2021-04-22 18:42:40 +0200

    fixed error on homepage controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,14 @@
 
 namespace App\Controller;
 
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
 class HomeController extends AbstractController {
+
+    /**
+     * @Route("/", name="homepage")
+     */
     public function homepage () {
         return $this->render('homepage.html.twig');
     }
