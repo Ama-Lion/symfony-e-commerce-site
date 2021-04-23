2fb1ef25674e4b0300b129088378a38491c96e53
‘Ama_lion’ <bahamadu2019@gmail.com>
2021-04-23 15:42:24 +0200

    added faker extensions

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,23 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use Faker\Factory;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Factory::create('fr_FR');
+
+        for($i = 0; $i < 75; $i++){
+            $product = new Product();
+            $product->setName($faker->sentence())
+                    ->setPrice(mt-rand(100, 200))
+                    ->setSlug($faker->slug());
+            $manager->persist($product);
+        }
 
         $manager->flush();
     }
