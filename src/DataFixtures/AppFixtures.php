fa988321568e68f7ec055ddef2a462db118836b3
‘Ama_lion’ <bahamadu2019@gmail.com>
2021-04-23 15:55:06 +0200

    addded fake fixtures data

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use Faker\Factory;
+use App\Entity\Product;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 
@@ -11,11 +12,13 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
         $faker = Factory::create('fr_FR');
+        $faker->addProvider(new \Liior\Faker\Prices($faker));
+        $faker->addProvider(new \Bezhanov\Faker\Provider\Commerce($faker));
 
         for($i = 0; $i < 75; $i++){
             $product = new Product();
-            $product->setName($faker->sentence())
-                    ->setPrice(mt-rand(100, 200))
+            $product->setName($faker->productName)
+                    ->setPrice($faker->price(4000, 20000))
                     ->setSlug($faker->slug());
             $manager->persist($product);
         }
