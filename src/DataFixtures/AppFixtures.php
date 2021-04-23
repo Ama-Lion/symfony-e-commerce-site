3e60afd0b1a5b89d654596d1778d1ee98caecb2c
‘Ama_lion’ <bahamadu2019@gmail.com>
2021-04-23 17:26:51 +0200

    added picture and description field fixtures and added picsuip extension for faker

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -23,6 +23,7 @@ class AppFixtures extends Fixture
         $faker = Factory::create('fr_FR');
         $faker->addProvider(new \Liior\Faker\Prices($faker));
         $faker->addProvider(new \Bezhanov\Faker\Provider\Commerce($faker));
+        $faker->addProvider(new \Bluemmb\Faker\PicsumPhotosProvider($faker));
 
         for($c = 0; $c < 3; $c++) {
             $category = new Category;
@@ -34,7 +35,9 @@ class AppFixtures extends Fixture
                 $product->setName($faker->productName)
                         ->setPrice($faker->price(4000, 20000))
                         ->setSlug(strtolower($this->slugger->slug($product->getName())))
-                        ->setCategory($category);
+                        ->setCategory($category)
+                        ->setDescription($faker->paragraph)
+                        ->setPicture($faker->imageUrl(400, 400, true));
                 $manager->persist($product);
             }
         }
