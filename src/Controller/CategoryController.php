7ba21e335792451b6d19b9457393cb92c34e2ea8
‘Ama_lion’ <bahamadu2019@gmail.com>
2021-04-24 18:24:13 +0200

    added create category form and function

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Form\CategoryType;
 use App\Repository\CategoryRepository;
-use Symfony\Component\HttpFoundation\Response;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CategoryController extends AbstractController
@@ -12,28 +16,38 @@ class CategoryController extends AbstractController
     /**
      * @Route("admin/categories", name="categories")
      */
-    public function viewCategories (CategoryRepository $categoryRepository)
+    public function viewCategories 
+    (
+        CategoryRepository $categoryRepository,
+        Category $category = null,
+        request $request,
+        EntityManagerInterface $manager,
+        SluggerInterface $slugger
+    )
     {
         $categories = $categoryRepository->findAll();
         if(!$categories){
             throw $this->createNotFoundException("Category not found mehnnn");
         }
-        return $this->render('category/view_categories.html.twig', [
-            'categories' => $categories,
-        ]);
-    }
 
-    /**
-     * @Route("admin/category/create", name="create_category")
-     */
-    public function createCategories (CategoryRepository $categoryRepository)
-    {
-        $categories = $categoryRepository->findAll();
-        if(!$categories){
-            throw $this->createNotFoundException("Category not found mehnnn");
+        //Create category form
+        $category= new Category();
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $category->setSlug(strtolower($slugger->slug($category->getName())));
+
+            $manager->persist($category);
+            $manager->flush();
+            $this->addFlash('success', 'Category' . ' ' .$category->getName() . ' '. 'created successfully');
+            return $this->redirectToRoute('categories');
         }
+
+
         return $this->render('category/view_categories.html.twig', [
             'categories' => $categories,
+            'form' => $form->createView()
         ]);
     }
 
