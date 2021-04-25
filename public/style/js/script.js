ca705f40b03dcc99ca86ac35221a9c32464197c4
‘Ama_lion’ <bahamadu2019@gmail.com>
2021-04-25 22:08:57 +0200

    updated hero content

--- public/style/js/script.js
+++ public/style/js/script.js
@@ -3,11 +3,7 @@
 // jquery ready start
 $(document).ready(function() {
 	// jQuery code
-
-  // var html_download = '<a href="http://bootstrap-ecommerce.com/templates.html" class="btn btn-dark rounded-pill" style="font-size:13px; z-index:100; position: fixed; bottom:10px; right:10px;">Download theme</a>';
-  //  $('body').prepend(html_download);
-    
-
+	
 	//////////////////////// Prevent closing from click inside dropdown
     $(document).on('click', '.dropdown-menu', function (e) {
       e.stopPropagation();
