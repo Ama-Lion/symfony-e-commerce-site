c99dc1d758b8248924c0456378ed7b028f42d321
‘Ama_lion’ <bahamadu2019@gmail.com>
2021-04-25 18:05:55 +0200

    added login authentication with form and functions

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,19 +2,34 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\LoginType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
     /**
-     * @Route("/security", name="security")
+     * @Route("/login", name="security_login")
      */
-    public function index(): Response
+    public function login(AuthenticationUtils $utils): Response
     {
-        return $this->render('security/index.html.twig', [
-            'controller_name' => 'SecurityController',
+        $form = $this->createForm(LoginType::class);
+        
+        // if ($form->isSubmitted() && $form->isValid()) {
+        //     $this->addFlash('success', 'Logged in successfully');
+        // }
+    
+        return $this->render('security/login.html.twig',[
+            'form' => $form->createView(),
+            'error' => $utils->getLastAuthenticationError(),
         ]);
     }
+
+    /**
+     * @Route("/logout", name="security_logout")
+     */
+    public function logout(){}
+
 }
