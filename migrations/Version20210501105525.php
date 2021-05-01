7f38cd206329d70ae7d31f917811bcf6798cdef6
Amadou BAH <51505362+Ama-Lion@users.noreply.github.com>
2021-05-01 13:25:04 +0200

    added different user profile templates

--- migrations/Version20210501105525.php
+++ migrations/Version20210501105525.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210425174410 extends AbstractMigration
+final class Version20210501105525 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -20,7 +20,8 @@ final class Version20210425174410 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        // $this->addSql('ALTER TABLE product ADD owner_id INT NOT NULL');
+        $this->addSql('CREATE TABLE purchase (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, name VARCHAR(255) NOT NULL, address VARCHAR(255) NOT NULL, postal_code VARCHAR(255) NOT NULL, city VARCHAR(255) NOT NULL, total INT NOT NULL, status VARCHAR(255) NOT NULL, INDEX IDX_6117D13BA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE purchase ADD CONSTRAINT FK_6117D13BA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE product ADD CONSTRAINT FK_D34A04AD7E3C61F9 FOREIGN KEY (owner_id) REFERENCES user (id)');
         $this->addSql('CREATE INDEX IDX_D34A04AD7E3C61F9 ON product (owner_id)');
     }
@@ -28,8 +29,8 @@ final class Version20210425174410 extends AbstractMigration
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE purchase');
         $this->addSql('ALTER TABLE product DROP FOREIGN KEY FK_D34A04AD7E3C61F9');
         $this->addSql('DROP INDEX IDX_D34A04AD7E3C61F9 ON product');
-        $this->addSql('ALTER TABLE product DROP owner_id');
     }
 }
