a16761830b2a405527dbdfadeacd35df7e1c96df
Amadou BAH <51505362+Ama-Lion@users.noreply.github.com>
2021-05-01 22:18:48 +0200

    refractored cart controller

--- src/Controller/Purchase/PurchaseConfirmationController.php
+++ src/Controller/Purchase/PurchaseConfirmationController.php
@@ -10,30 +10,22 @@ use App\Form\CartConfirmationType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Security;
-use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Form\FormFactoryInterface;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class PurchaseConfirmationController extends AbstractController
 {
-    protected $formFactory;
     protected $router;
     protected $security;
     protected $cartService;
     protected $em;
 
     public function __construct(
-        FormFactoryInterface $formFactory,
-        RouterInterface $router,
         Security $security,
         CartService $cartService,
         EntityManagerInterface $em
     ) {
-        $this->formFactory = $formFactory;
-        $this->router = $router;
         $this->security = $security;
         $this->cartService = $cartService;
         $this->em = $em;
@@ -44,14 +36,14 @@ class PurchaseConfirmationController extends AbstractController
      */
     public function confirm(Request $request)
     {
-        $form = $this->formFactory->create(CartConfirmationType::class);
+        $form = $this->createForm(CartConfirmationType::class);
         $form->handleRequest($request);
         if (!$form->isSubmitted()) {
             $this->addFlash('warning', 'Please fill the form to validate your purchase');
-            return new RedirectResponse($this->router->generate('cart_show'));
+            return $this->redirectToRoute('cart_show');
         }
 
-        $user = $this->security->getUser();
+        $user = $this->getUser();
         if (!$user) {
             throw new AccessDeniedException('YOU NEED TO BE LOGGED IN TO CONFIRM A PURCHASE');
         }
@@ -59,7 +51,7 @@ class PurchaseConfirmationController extends AbstractController
         $cartItems = $this->cartService->getDetailedItems();
         if (count($cartItems) == 0) {
             $this->addFlash('warning', 'You can not confirm a purchase with empty products list');
-            return new RedirectResponse($this->router->generate('cart_show'));
+            return $this->redirectToRoute('cart_show');
         }
 
         /** @var Purchase */
@@ -86,6 +78,6 @@ class PurchaseConfirmationController extends AbstractController
         $this->em->flush();
         $this->addFlash('succes', 'Your Purchase was successfully processed');
 
-        return new RedirectResponse($this->router->generate('purchases_index'));
+        return $this->redirectToRoute('purchases_index');
     }
 }
