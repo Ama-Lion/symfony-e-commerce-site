334f8c844535b51cf1832a9fc4a3a632200aeea3
Amadou BAH <51505362+Ama-Lion@users.noreply.github.com>
2021-05-02 15:51:30 +0200

    added event listiners for purchase success emails

--- src/Controller/Purchase/PurchasePaymentSuccessController.php
+++ src/Controller/Purchase/PurchasePaymentSuccessController.php
@@ -4,9 +4,11 @@ namespace App\Controller\Purchase;
 
 use App\Entity\Purchase;
 use App\Cart\CartService;
+use App\Event\PurchaseSuccessEvent;
 use App\Repository\PurchaseRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PurchasePaymentSuccessController extends AbstractController
@@ -14,7 +16,13 @@ class PurchasePaymentSuccessController extends AbstractController
     /**
      * @Route("/purchase/terminate/{id}", name="purchase_payment_success")
      */
-    public function success($id, PurchaseRepository $purchaseRepository, EntityManagerInterface $em, CartService $cartService)
+    public function success(
+        $id, 
+        PurchaseRepository $purchaseRepository, 
+        EntityManagerInterface $em, 
+        CartService $cartService,
+        EventDispatcherInterface $dispatcher
+        )
     {
         $purchase = $purchaseRepository->find($id);
 
@@ -32,6 +40,10 @@ class PurchasePaymentSuccessController extends AbstractController
         $em->flush();
 
         $cartService->emptyCart();
+
+        $purchaseEvent = new PurchaseSuccessEvent($purchase);
+
+        $dispatcher->dispatch($purchaseEvent, 'purchase.success');
 
         $this->addFlash('success', 'Purchase Ordered successfully');
         return $this->redirectToRoute('purchases_index');
